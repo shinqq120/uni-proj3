7444f5d7c6c151a077d2f5b032225a5020087a54
shin <1208579634@qq.com>
2023-03-29 08:50:49 +0800

    完成分类页面

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,6 +1,29 @@
 <template>
   <view>
-    
+    <view class="scroll-view-contain">
+      <!-- 左侧的滚动视图区域 -->
+      <scroll-view class="left-scroll-view" scroll-y :style="{height:wh+'px'}">
+       <block v-for="(item,i) in cateList" :key="i">
+         <view :class="['left-scroll-view-item',i===active?'active':'']" @click="activeChanged(i)">{{item.cat_name}}</view>
+       </block>
+      </scroll-view>
+      <!-- 右侧的滚动视图区域  -->
+      <scroll-view class="right-scroll-view" scroll-y :style="{height:wh+'px'}" :scroll-top="scrollTop">
+        <view class="cate-lv2" v-for="(item2,i2) in cateLevel2" :key="i2">
+          <view class="cate-lv2-title">/{{item2.cat_name}}/</view>
+          <!-- 动态渲染三级分类的列表数据 -->
+          <view class="cate-lv3-list">
+            <!-- 三级分类Item项 -->
+            <view class="cate-lv3-item" v-for="(item3,i3) in item2.children" :key="i3" @click="gotoGoodsList(item3)">
+            <!-- 三级分类的图片 -->
+            <image :src="item3.cat_icon.replace('dev','web')"></image>
+            <!-- 文本 -->
+            <text>{{item3.cat_name}}</text>
+            </view>
+          </view>
+        </view>
+      </scroll-view>
+    </view>
   </view>
 </template>
 
@@ -8,12 +31,105 @@
   export default {
     data() {
       return {
-        
+        // wh窗口高度
+        wh:0,
+        // 分类数据列表
+        cateList:[],
+        // 当前选中项的索引，more第一项被选中
+        active:0,
+        // 二级分类列表
+        cateLevel2:[],
+        // 滚动条距离顶部距离
+        scrollTop:0
       };
+    },
+    onLoad(){
+      // 将当前系统的信息赋值给sysInfo常量
+      const sysInfo=uni.getSystemInfoSync()
+      // 为wh窗口的高度动态赋值
+      this.wh=sysInfo.windowHeight
+      // 调用获取分类列表数据的方法
+      this.getCateList()
+      
+    },
+    methods:{
+
+      activeChanged(i){
+        this.active=i
+        // 重新为二级分类列表重新赋值
+        this.cateLevel2=this.cateList[i].children
+        
+        this.scrollTop=this.scrollTop===0?1:0
+      },
+      async getCateList(){
+        const {data:res} =await uni.$http.get('/api/public/v1/categories')
+        if(res.meta.status!==200) return uni.$showMsg()
+        this.cateList=res.message
+        this.cateLevel2=res.message[0].children
+      },
+      gotoGoodsList(item3){
+        uni.navigateTo({
+          url:'/subpkg/goods_list/goods_list?cid=' +item3.cat_id
+        })
+      }
     }
   }
 </script>
 
 <style lang="scss">
-
+    .scroll-view-contain{
+      display: flex;
+      .left-scroll-view{
+        width: 120px;
+        .left-scroll-view-item{
+          line-height: 60px;
+          background-color: #f7f7f7;
+          text-align: center;
+          font-size: 12px;
+          &.active{
+            background-color: #ffffff;
+            position: relative;
+          
+          &::before{
+            content: '';
+            display:block;
+            width: 3px;
+            height: 30px;
+            background-color: #c00000;
+            position: absolute;
+            left:0;
+            top:50%;
+            transform: translateY(-50%);
+            }
+          }
+        }
+      }
+    }
+    .cate-lv2-title {
+      font-size: 12px;
+      font-weight: bold;
+      text-align: center;
+      padding: 15px 0;
+    }
+    .cate-lv3-list {
+      display: flex;
+      flex-wrap: wrap;
+    
+      .cate-lv3-item {
+        width: 33.33%;
+        margin-bottom: 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    
+        image {
+          width: 60px;
+          height: 60px;
+        }
+    
+        text {
+          font-size: 12px;
+        }
+      }
+    }
 </style>
